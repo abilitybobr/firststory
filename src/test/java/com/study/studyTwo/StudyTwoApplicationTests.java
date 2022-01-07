23c5ee151441891288f240e100da6504dd502519
young <>
2022-01-07 14:38:02 +0900

    first study story init commit

---
+++ src/test/java/com/study/studyTwo/StudyTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.studyTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
